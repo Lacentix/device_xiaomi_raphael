522795e0bbeba6db0e8ddd0e511913f429792023
TheScarastic <warabhishek@gmail.com>
2021-10-06 07:25:13 +0000

    raphael: motor: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to prefernce screen
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Change-Id: Ib5bc3e8ff69e603a063c7d8116ded6a19956ad32

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -18,17 +18,21 @@
 package org.lineageos.settings.popupcamera;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 
-public class PopupCameraSettingsActivity extends PreferenceActivity {
 
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
     private static final String TAG_POPUPCAMERA = "popupcamera";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new PopupCameraSettingsFragment(), TAG_POPUPCAMERA).commit();
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, new PopupCameraSettingsFragment(), TAG_POPUPCAMERA)
+                .commit();
     }
 }
